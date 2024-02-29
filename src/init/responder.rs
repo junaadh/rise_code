abd46bc8e6f404dd65d2d633624e4ae477a1fd29
Junaadh <junaadh.02@gmail.com>
2024-03-01 06:14:10 +0800

    added linux specific scripts

--- src/init/responder.rs
+++ src/init/responder.rs
@@ -2,6 +2,7 @@ use std::io::Write;
 
 use crate::traits::{DirCreator, DirPerms, UnwrapAndLogRes};
 
+#[cfg(target_os = "macos")]
 fn startup() -> String {
     r#"#!/bin/sh
         
@@ -15,6 +16,29 @@ fn startup() -> String {
 
         hx_window=$(tmux list-windows -t $session_name -F '#{window_index} #{window_name}' | grep hx | cut -d' ' -f1)
         echo $session_name:$language:$file_name:$repo | nc -U /tmp/dev_rpc
+        exit 0
+    else
+        echo $session_name | nc -U /tmp/dev_rpc
+        exit 0
+    fi"#
+    .to_string()
+    //TODO: if git is not a ssh authenticated
+}
+
+#[cfg(target_os = "linux")]
+fn startup() -> String {
+    r#"#!/bin/sh
+        
+    type=$1
+    session_name=$2
+        
+    if [[ $1 == "attach" ]]; then
+        path=$3
+        language=$(echo $path | cut -d'/' -f5)
+        repo=$(git --git-dir=$path/.git config --get remote.origin.url | cut -d':' -f2)
+
+        hx_window=$(tmux list-windows -t $session_name -F '#{window_index} #{window_name}' | grep hx | cut -d' ' -f1)
+        echo $session_name:$language:$file_name:$repo | socat - UNIX-CONNECT:/tmp/dev_rpc
         exit 0
     else
         echo $session_name | nc -U /tmp/dev_rpc
